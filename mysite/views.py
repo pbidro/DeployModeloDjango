10ac1e635b7835f406db6c1d032275745c42261d
Pablo Iván Bustamante Idro <pb.idro94@gmail.com>
2022-11-29 22:07:33 -0300

    carga modelo

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render, redirect
+import pickle5 as pickle
+
 
 def inicio(request):
-    return render(request,"index.html")
+    loaded_model = pickle.load(open("finalized_model.sav", 'rb'))
+    return render(request,"index.html",loaded_model)
+
+def modelo(request):
+    loaded_model = pickle.load(open("finalized_model.sav", 'rb'))
